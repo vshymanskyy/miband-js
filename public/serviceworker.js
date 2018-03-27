7472f44a8f23543dec88787c2da220c3de3fca92
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2018-03-28 00:05:21 +0300

    Add service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,22 @@
-/* TODO */
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('miband').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/favicon.png',
+       '/webapp.bundle.js',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
