deaafeb52300a6a9da4f019ef9254c91c494e9b4
Alex Bardanov <dnbard@gmail.com>
2018-04-02 19:52:55 +0300

    #2 service worker prototype

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,22 +1,27 @@
-
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('miband').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/favicon.png',
-       '/webapp.bundle.js',
-     ]);
-   })
- );
+  e.waitUntil(
+    caches.open('miband').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.png',
+        '/webapp.bundle.js'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
+
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    fetch(event.request).then(function(response){
+      return caches.open('miband').then(function(cache){
+        cache.put(event.request.url, response.clone());
+        return response;
+      });
+    }).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
